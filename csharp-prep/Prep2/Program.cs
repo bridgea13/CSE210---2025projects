d81f6b0d804b3e87470c1aab935609fe1853d4c5
bridgea13 <bri21025@byui.edu>
2025-01-11 22:36:30 -0500

    preps 1 and 2 successful

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+
+        int x = int.Parse(gradePercentage);
+        string letter ="";
+        
+
+        if (x >= 90)
+        {
+            letter = "A";
+        }
+        else if (x >= 80)
+        {
+            letter = "B";
+        }
+        else if (x >= 70)
+        {
+            letter = "C";
+        }
+        else if (x >= 60)
+        {
+            letter = "D";;
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine("Your grade is a "+letter);
+
+        if (x >= 70)
+        {
+            Console.WriteLine("You Passed!!!");
+        }
+        else{
+            Console.WriteLine("You failed.");
+        }
     }
 }
