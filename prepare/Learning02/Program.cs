78a6b12e52c87b639869b09518c28b08857be851
bridgea13 <bri21025@byui.edu>
2025-01-18 12:40:04 -0500

    week 3 learn assignment complete

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._jobCompany = "Salesforce";
+        job1._jobStartYear = 2020;
+        job1._jobEndYear = 2025;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._jobCompany = "Apple";
+        job2._jobStartYear = 1999;
+        job2._jobEndYear = 2020;
+
+        Resume myResume = new Resume();
+        myResume._name = "Bing Bong"; 
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
