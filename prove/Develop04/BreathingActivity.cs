8866eb109cdda8e56bb0310767d2fb929a6e9c01
bridgea13 <bri21025@byui.edu>
2025-03-04 00:10:51 -0500

    breathing working

---
+++ prove/Develop04/BreathingActivity.cs
@@ -0,0 +1,52 @@
+class BreathingPick
+{
+    public void Pick1()
+    {
+        Console.WriteLine("This activity is great for relaxation. You can choose for how long to participate in some breathing exercises. Just follow along and we will tell you what to do.");
+        Thread.Sleep(3000);
+        Console.WriteLine("Enter the amount of time in seconds that you would like to participate: ");
+
+        if (int.TryParse(Console.ReadLine(), out int duration))
+        {
+            int passingTime = 0;
+            Console.WriteLine("Let's begin.");
+            Thread.Sleep(1000);
+            Console.WriteLine($"Starting countdown...");
+
+            while (passingTime < duration)
+            {
+                if (passingTime + 5 <= duration)
+                {
+                    Console.WriteLine("Big breath in...");
+                    for (int i =5; i > 0; i--)
+                    {
+                        Console.WriteLine(i);
+                        Thread.Sleep(1000);
+                    }
+                    passingTime += 5;
+                } else {
+                    break;
+                }
+
+                if (passingTime + 5 <= duration)
+                {
+                    Console.WriteLine("Now breath out...");
+                    for (int i =5; i > 0; i--)
+                    {
+                        Console.WriteLine(i);
+                        Thread.Sleep(1000);
+                    }
+                    passingTime += 5;
+                } else {
+                    break;
+                }
+            
+            }
+
+            Console.WriteLine("Done.");
+            Thread.Sleep(2000);
+        } else {
+            Console.WriteLine("The amount of time in seconds is not valid, please try again.");
+        }
+    }
+}
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,87 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        bool running = true;
+
+        BreathingPick breathingPick = new BreathingPick();
+        //ListingPick listingPick = new ListingPick();
+        //ReflectingPick reflectingPick = new ReflectingPick();
+        //MovementPick movementPick = new MovementPick();
+
+        while (running)
+        {
+            Console.WriteLine("Welcome! We are here to help you be more mindful.");
+            Thread.Sleep(1000);
+            Console.WriteLine("\nPick an activity from the following options below: ");
+            Console.WriteLine("1. Breathing");
+            Console.WriteLine("2. Listing");
+            Console.WriteLine("3. Reflecting");
+            Console.WriteLine("4. Movement");
+            Console.WriteLine("5. Quit");
+            Console.WriteLine("\nEnter the number of your choice.");
+
+            string userPick = Console.ReadLine();
+
+            
+
+            switch (userPick)
+            {
+                case "1":
+                    Console.WriteLine("Great! Let's do a breathing activity...");
+                    breathingPick.Pick1();
+                    break;
+                case "2":
+                    Console.WriteLine("Great! Let's do a listing activity...");
+                    //listingPick.Pick2();
+                    break;
+                case "3":
+                    Console.WriteLine("Great! Let's do a reflecting activity...");
+                    //reflectingPick.Pick3();
+                    break;
+                case "4":
+                    Console.WriteLine("Great! Let's do a movement activity...");
+                    //movementPick.Pick4();
+                    break;
+                case "5":
+                    Console.WriteLine("Thank you for being mindful.");
+                    return;
+                default:
+                    break;
+            }
+
+            List<string> animationStrings =
+            [
+                "|",
+                "/",
+                "-",
+                "\\",
+                "|",
+                "/",
+                "-",
+                "\\",
+            ];
+
+            foreach (string s in animationStrings)
+            {
+                Console.Write(s);
+                Thread.Sleep(1000);
+                Console.Write("\b \b");
+            }
+
+            DateTime startTime = DateTime.Now;
+            DateTime endTime = startTime.AddSeconds(10);
+
+            while (DateTime.Now < endTime)
+            {
+                Console.Write(".");
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Done.");
+        }
     }
 }
